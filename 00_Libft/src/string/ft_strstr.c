635328040201a733e352a4d102cc059861eb953e
jjongs2 <jjongs@hanyang.ac.kr>
2022-09-03 00:56:10 +0900

    Add `ft_strstr()`

--- 00_Libft/src/string/ft_strstr.c
+++ 00_Libft/src/string/ft_strstr.c
@@ -1,25 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strnstr.c                                       :+:      :+:    :+:   */
+/*   ft_strstr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: kijsong <kijsong@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/07/06 11:04:30 by kijsong           #+#    #+#             */
-/*   Updated: 2022/09/03 00:52:58 by kijsong          ###   ########.fr       */
+/*   Created: 2022/09/02 13:40:36 by kijsong           #+#    #+#             */
+/*   Updated: 2022/09/03 00:15:18 by kijsong          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/ft_string.h"
 
-char	*ft_strnstr(const char *s1, const char *s2, size_t n)
+char	*ft_strstr(const char *s1, const char *s2)
 {
 	size_t	s2_len;
 
 	if (!*s2)
 		return ((char *)s1);
 	s2_len = ft_strlen(s2);
-	while (*s1 && s2_len <= n--)
+	while (*s1)
 	{
 		if (!ft_strncmp(s1, s2, s2_len))
 			return ((char *)s1);
